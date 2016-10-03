375de104a7f79151dc0d6093fb78980291beaaef
Philip Kinlen <philipkinlen@hotmail.com>
2016-10-03 16:34:05 +0100

    Add files via upload

--- Sep2016/PonderThis.java
+++ Sep2016/PonderThis.java
@@ -15,7 +15,6 @@ public class PonderThis {
      
      ////////////////////////////////////////////////////
      public static Solution solve(){
-    	     	     	 
     	 m_counter = 0;
     	 
     	 boolean  finished           = false;
@@ -23,15 +22,15 @@ public class PonderThis {
     	 Solution finalSolution      = null;
     	 
     	 while ( ! finished ){
-    	     System.out.println(   "About to start, the number of allowed flaws is: " + Integer.toString(numAllowedFlaws) 
-                                 + " \tCounter: "                                     + Long.toString(m_counter)          );       
+    	     System.out.println(   "The number of allowed flaws is: " + Integer.toString(numAllowedFlaws) 
+                                 + " \tcounter: "                                     + Long.toString(m_counter)          );       
              
     		 finalSolution = solve(new Solution(numAllowedFlaws));
 
     		 if ( finalSolution != null){
+    			 finalSolution.print();
     			 System.out.println("Found solution, number of allowed flaws is: "+ Integer.toString(numAllowedFlaws));
     			 System.out.println("Number of steps: " + Long.toString(m_counter));
-    			 finalSolution.print();
                  finalSolution.printTranspose();
     			 
     			 finished = true;
@@ -46,13 +45,11 @@ public class PonderThis {
      // The following function is recursive.
      public static Solution solve(Solution s){
      
-    	if ( s.m_current == (Solution.m_length - 1))
+    	if ( s.getCurrent() == (Solution.m_length - 1))
     		return s; // we have a solution
     	 
-    	int i = 0;
-    	    	
+    	int i = 0;    	    	
     	while ( i < Solution.m_length) {
-    		
     		
     		if ( s . isAllowed(i)){
     			
@@ -63,14 +60,16 @@ public class PonderThis {
     		}
     		
     		i++;
+            m_counter++;    
     		//////////////////////////////////////////////////////////////////
     		// We now have a bit of code that might be helpful for debugging:
-            m_counter++;    
+            /*
             //if ( true) {
-            if ( m_counter % 10000L == 0L) {
-            	System.out.println("counter: " + Long.toString(m_counter) + ", current: " + Integer.toString(s . m_current));
+            if ( m_counter % 100000L == 0L) {
+            	System.out.println("counter: " + Long.toString(m_counter) + ", current: " + Integer.toString(s . getCurrent()));
                 s . print();
             }
+            */
     	}
         return null; // i.e. a solution not found.    	 
     	     		
